b277f45474f430721bf082e2e204bbc4631ba809
Samuel Gonçalves Cunha <samukasaww@live.com>
2015-04-26 14:21:52 -0300

    Atualizaçao
    Adicionado a funçao 'apuracao'.

--- Estrutura de Dados II/TP I/ControleEleitoral/ControleEleitoral.h
+++ Estrutura de Dados II/TP I/ControleEleitoral/ControleEleitoral.h
@@ -9,5 +9,6 @@ void* inicializaTabela(unsigned int tipo, unsigned int tamanho);
 void terminaTabelasEleicao(void *tabela, unsigned int tipo, THashLinear prefeito, THashLinear vereador);
 int inserirVotoPrefeito(void *tabela, unsigned int tipo, cedulaEleitoral cedula, THashLinear prefeito);
 int inserirVotoVereador(void *tabela, unsigned int tipo, cedulaEleitoral cedula, THashLinear vereador);
+void apuracao(THashLinear prefeito, THashLinear vereador);
 int removerEleitor(void *tabela, unsigned int tipo, cedulaEleitoral cedula, THashLinear prefeito, THashLinear vereador);
 #endif
